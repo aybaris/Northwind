844164c165c4f73a1e28017360538e16bbaa8fa4
aybaris <83760836+aybaris@users.noreply.github.com>
2021-06-29 15:31:06 +0300

    Northwind - 8th Week

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,11 +2,31 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
+
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
-	Result add(Product product);
+
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
+	
+	DataResult<List<Product>> getAllSorted ();
+	
+	Result add(Product product); 
+
+	DataResult<Product> getByProductName(String productName);
+
+	DataResult<Product> getByProductNameAndCategoryId(String productName, int categoryId); // and denildiği için ikisine de																					// uyulacak
+
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName, int categoryId); // or kullanıldığı için ikisinden
+																					// birisine bakacak
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);// id'ye göre getirme
+
+	DataResult<List<Product>> getByProductNameContains(String productName); // ürün ismine göre arama
+
+	DataResult<List<Product>> getByProductNameStartsWith(String productName); // ürün ismine şununla başlayan gibi fonklar
+
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
 }
--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,9 +1,26 @@
 package kodlamaio.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer>{
+	Product getByProductName(String productName);
+	
+	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId); // and denildiği için ikisine de uyulacak
+	
+	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId); // or kullanıldığı için ikisinden birisine bakacak
+	
+	List<Product> getByCategory_CategoryIdIn(List<Integer> categories);//id'ye göre getirme
+	
+	List<Product> getByProductNameContains(String productName); //ürün ismine göre arama
 
+	List<Product> getByProductNameStartsWith(String productName); //ürün ismine şununla başlayan gibi fonklar
+	
+	@Query("From Product where productName=:productName and category.categoryId=:categoryId")//JPQL YAZIMI
+	List<Product> getByNameAndCategory(String productName, int categoryId);
+	
 }
